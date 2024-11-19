263acaa8ba39fef52fde71138352154d73bfdd8c
vikash1509 <vikashbansal1509@gmail.com>
2024-11-19 22:05:31 +0530

    first-commit

---
+++ examportal-frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header name="vikash" color="green"/>
+      <hr/>
+      <Header name="Bansal" color="red"/>
+      <hr/>
+      <h1>asdfghjkl sdfghjkl</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ examportal-frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+function Header({name}) {
+    return (
+        <div style={{background: "green" , padding: 20} }>
+            <h1>Hii {name} welconme back </h1>
+        </div>
+    );
+}
+
+
+export default Header;
